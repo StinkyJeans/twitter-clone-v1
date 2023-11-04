e1fbf0833a277a3728e89c940a89dd9114c76f54
StinkyJeans <johncona3@gmail.com>
2023-11-05 00:05:08 +0800

    updated to be centered

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home({newsResults, randomUsersResults}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='flex min-h-screen mx-auto pt-1'>
+      <main className='flex min-h-screen max-w-7xl mx-auto pt-1 '>
 
         {/* Sidebar */}
 
